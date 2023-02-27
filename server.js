0b4a9c4c96e85a48fd0057f75fb36ab2a226a595
Jullian0426 <jullian0426@yahoo.com>
2023-02-27 05:46:30 -0600

    Write .sql files to create and seed database. Create connection in server.js

--- server.js
+++ server.js
@@ -1,4 +1,31 @@
+// Require dependencies
 require('dotenv').config();
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+// Connect to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_,
+    database: 'employees_db'
+});
+
+
+
+// =================================================
+//   ______                 _                       
+//  |  ____|               | |                      
+//  | |__   _ __ ___  _ __ | | ___  _   _  ___  ___ 
+//  |  __| | '_ ` _ \| '_ \| |/ _ \| | | |/ _ \/ _ \
+//  | |____| | | | | | |_) | | (_) | |_| |  __/  __/
+//  |______|_| |_| |_| .__/|_|\___/ \__, |\___|\___|
+//  |  \/  |         | |             __/ |          
+//  | \  / | __ _ _ _|_| __ _  __ _ |___/_ __       
+//  | |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|      
+//  | |  | | (_| | | | | (_| | (_| |  __/ |         
+//  |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|         
+//                             __/ |                
+//                            |___/                 
+// =================================================
