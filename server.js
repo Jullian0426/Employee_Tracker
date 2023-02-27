acd55c167ceec1354bf6e5ebb152b743af4ada91
Jullian0426 <jullian0426@yahoo.com>
2023-02-27 10:29:48 -0600

    Write viewAllEmployees function and fix bugs

--- server.js
+++ server.js
@@ -8,14 +8,11 @@ const cTable = require('console.table');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: process.env.DB_USER,
-    password: process.env.DB_,
+    password: process.env.DB_PASSWORD,
     database: 'employees_db'
 });
 
 function initPrompt () {
-    console.log(
-        "=================================================\r\n  ______                 _                       \r\n |  ____|               | |                      \r\n | |__   _ __ ___  _ __ | | ___  _   _  ___  ___ \r\n |  __| | \'_ ` _ \\| \'_ \\| |\/ _ \\| | | |\/ _ \\\/ _ \\\r\n | |____| | | | | | |_) | | (_) | |_| |  __\/  __\/\r\n |______|_| |_| |_| .__\/|_|\\___\/ \\__, |\\___|\\___|\r\n |  \\\/  |         | |             __\/ |          \r\n | \\  \/ | __ _ _ _|_| __ _  __ _ |___\/_ __       \r\n | |\\\/| |\/ _` | \'_ \\ \/ _` |\/ _` |\/ _ \\ \'__|      \r\n | |  | | (_| | | | | (_| | (_| |  __\/ |         \r\n |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|         \r\n                            __\/ |                \r\n                           |___\/                 \r\n================================================="
-    );
     
     inquirer
         .prompt({
@@ -33,7 +30,7 @@ function initPrompt () {
             "Quit"
         ]})
         .then((answer) => {
-            switch (answer) {
+            switch (answer.prompt) {
                 case "View All Employees":
                     viewAllEmployees();
                     break;
@@ -59,12 +56,37 @@ function initPrompt () {
                     break;
 
                 case "Add Department":
-                    viewAllEmployees();
+                    addDepartment();
                     break;
 
                 case "Quit":
+                    connection.end();
                     break;
             }
         })
 };
 
+function viewAllEmployees() {
+    const sql = `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employees e
+    JOIN roles r
+      ON e.role_id = r.id
+    JOIN departments d
+    ON d.id = r.department_id
+    LEFT JOIN employees m
+      ON m.id = e.manager_id`;
+
+    connection.query(sql, (err, res) => {
+        if (err) {
+            throw err;
+        }
+        console.table(res);
+        initPrompt();
+    })
+};
+
+console.log(
+    "=================================================\r\n  ______                 _                       \r\n |  ____|               | |                      \r\n | |__   _ __ ___  _ __ | | ___  _   _  ___  ___ \r\n |  __| | \'_ ` _ \\| \'_ \\| |\/ _ \\| | | |\/ _ \\\/ _ \\\r\n | |____| | | | | | |_) | | (_) | |_| |  __\/  __\/\r\n |______|_| |_| |_| .__\/|_|\\___\/ \\__, |\\___|\\___|\r\n |  \\\/  |         | |             __\/ |          \r\n | \\  \/ | __ _ _ _|_| __ _  __ _ |___\/_ __       \r\n | |\\\/| |\/ _` | \'_ \\ \/ _` |\/ _` |\/ _ \\ \'__|      \r\n | |  | | (_| | | | | (_| | (_| |  __\/ |         \r\n |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|         \r\n                            __\/ |                \r\n                           |___\/                 \r\n================================================="
+);
+
+initPrompt();
