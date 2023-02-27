87e1aecfcf7ae85350cd6c6833002fbed23af0e1
Jullian0426 <jullian0426@yahoo.com>
2023-02-27 06:27:13 -0600

    Write initPrompt function with list prompt and a switch statement for handling answers

--- server.js
+++ server.js
@@ -12,20 +12,59 @@ const connection = mysql.createConnection({
     database: 'employees_db'
 });
 
+function initPrompt () {
+    console.log(
+        "=================================================\r\n  ______                 _                       \r\n |  ____|               | |                      \r\n | |__   _ __ ___  _ __ | | ___  _   _  ___  ___ \r\n |  __| | \'_ ` _ \\| \'_ \\| |\/ _ \\| | | |\/ _ \\\/ _ \\\r\n | |____| | | | | | |_) | | (_) | |_| |  __\/  __\/\r\n |______|_| |_| |_| .__\/|_|\\___\/ \\__, |\\___|\\___|\r\n |  \\\/  |         | |             __\/ |          \r\n | \\  \/ | __ _ _ _|_| __ _  __ _ |___\/_ __       \r\n | |\\\/| |\/ _` | \'_ \\ \/ _` |\/ _` |\/ _ \\ \'__|      \r\n | |  | | (_| | | | | (_| | (_| |  __\/ |         \r\n |_|  |_|\\__,_|_| |_|\\__,_|\\__, |\\___|_|         \r\n                            __\/ |                \r\n                           |___\/                 \r\n================================================="
+    );
+    
+    inquirer
+        .prompt({
+        type: "list",
+        name: "prompt",
+        message: "Would you like to do?",
+        choices: [
+            "View All Employees",
+            "Add Employee",
+            "Update Employee Role",
+            "View All Roles",
+            "Add Role",
+            "View All Departments",
+            "Add Department",
+            "Quit"
+        ]})
+        .then((answer) => {
+            switch (answer) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                
+                case "Add Employee":
+                    AddEmployee();
+                    break;
+            
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                    
+                case "View All Roles":
+                    viewAllRoles();
+                    break;
+                
+                case "Add Role":
+                    addRole();
+                    break;
 
+                case "View All Departments":
+                    viewAllDepartments();
+                    break;
 
-// =================================================
-//   ______                 _                       
-//  |  ____|               | |                      
-//  | |__   _ __ ___  _ __ | | ___  _   _  ___  ___ 
-//  |  __| | '_ ` _ \| '_ \| |/ _ \| | | |/ _ \/ _ \
-//  | |____| | | | | | |_) | | (_) | |_| |  __/  __/
-//  |______|_| |_| |_| .__/|_|\___/ \__, |\___|\___|
-//  |  \/  |         | |             __/ |          
-//  | \  / | __ _ _ _|_| __ _  __ _ |___/_ __       
-//  | |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '__|      
-//  | |  | | (_| | | | | (_| | (_| |  __/ |         
-//  |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_|         
-//                             __/ |                
-//                            |___/                 
-// =================================================
+                case "Add Department":
+                    viewAllEmployees();
+                    break;
+
+                case "Quit":
+                    break;
+            }
+        })
+};
+
